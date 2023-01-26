685b031599ce424bece2e1a86a16b46be7a50a4f
Ns4030 <sipos.bence-2018@keri.mako.hu>
2023-01-26 12:38:30 +0100

    Ódry made things

--- js/app.js
+++ js/app.js
@@ -19,6 +19,7 @@
         .state('order', {
           url: '/order',
           templateUrl: './html/order.html',
+          controller:"orderController"
         })
         .state('about', {
           url: '/about',
@@ -66,26 +67,68 @@
     $rootScope.state = { id: null, prev: null };
     $rootScope.user = { id: null, type: null, name: null };
 
-    // Get Flies
-    http
-      .request({
-        url: "./php/get.php",
-        method: "POST",
-        data: {
-          db: "opd",
-          query: "SELECT * FROM `menu`;",
-          isAssoc: true,
-        },
-      })
-      .then((data) => {
-        $rootScope.order = data;
-        $rootScope.$applyAsync();
-      })
-      .catch((e) => console.log(e));
+    
   },
 ])
 
-// Page2 controller
+.controller('orderController', ['$scope', 'http', function($scope, http) {
+  
+  // Get Flies
+  http
+  .request({
+    url: "./php/get.php",
+    method: "POST",
+    data: {
+      db: "opd",
+      query: "SELECT * FROM `menu`",
+      isAssoc: true,
+    },
+  })
+  .then((data) => {
+    $scope.order = data;
+    $scope.$applyAsync();
+    $scope.filter = null;
+    $scope.categories = [
+      {
+        id:"1",
+        name: 'Pizzák',
+        icon: 'fa-solid fa-pizza-slice'
+      },
+      {
+        id:"2",
+        name: 'Hamburgerek',
+        icon: 'fa-solid fa-burger'
+      },
+      {
+        id:"4",
+        name: 'Italok',
+        icon: 'fa-solid fa-glass-water'
+      },
+      {
+        id:"3",
+        name: 'StreetFood',
+        icon: 'fa-solid fa-bowl-food'
+      },
+      {
+        id:"6",
+        name: 'Köretek',
+        icon: 'fa-solid fa-bowl-rice'
+      },
+      {
+        id:"5",
+        name: 'Desszert',
+        icon: 'fa-solid fa-cookie-bite'
+      },
+      
+    ]
+    $scope.orderFilter = (event) => {
+      let element = event.currentTarget;
+      $scope.filter = element.id;
+      $scope.$applyAsync();
+
+    };
+  })
+  .catch((e) => console.log(e));
+}])
+
 })(window, angular);
-
-
