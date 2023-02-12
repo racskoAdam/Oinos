1cf6307745d6164adc78e40807b09f772b6b271c
Konyhasneni22 <balla.istvan-2018@keri.mako.hu>
2023-02-12 19:30:33 +0100

    otthoni

--- php/submit-form.php
+++ php/submit-form.php
@@ -4,20 +4,17 @@ $host = "localhost";
 $user = "root";
 $password = "";
 $db = "opd";
-
 $con = mysqli_connect($host, $user, $password, $db);
-$con -> set_charset("utf-8");
 
 if (!$con) {
     die("Connection failed: " . mysqli_connect_error());
   }
-  $id = mysqli_real_escape_string($con, 'id');
   $name = mysqli_real_escape_string($con, $_POST['name']);
   $email = mysqli_real_escape_string($con, $_POST['email']);
   $phone = mysqli_real_escape_string($con, $_POST['phone']);
   $date_time = mysqli_real_escape_string($con, $_POST['date_time']);
   
-  $sql = "INSERT INTO reservations (id, name, email, phone, date_time) VALUES ('$id','$name', '$email', '$phone', '$date_time')";
+  $sql = "INSERT INTO reservations ( name, email, phone, date_time) VALUES ('$name', '$email', '$phone', '$date_time')";
   
   if (mysqli_query($con, $sql)) {
     echo "New record created successfully";
