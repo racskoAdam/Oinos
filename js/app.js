e293f7936f4f4fb7764cb67bd6c418997d3447a8
Konyhasneni22 <balla.istvan-2018@keri.mako.hu>
2023-02-14 14:41:22 +0100

    fa

--- js/app.js
+++ js/app.js
@@ -31,6 +31,7 @@
           .state("faq", {
             url: "/faq",
             templateUrl: "./html/faq.html",
+            controller: "faqController",
           })
           .state("restaurant", {
             url: "/restaurant",
@@ -136,10 +137,13 @@
           url: "./php/submit-form.php", // URL to submit the form data to
           data: $.param($scope.formData), // Convert the formData object to URL-encoded string
           headers: { "Content-Type": "application/x-www-form-urlencoded" }, // Set header content type to application/x-www-form-urlencoded
-        }).then(function (response) {
-          if (response.data === "Ezzel a telefonszámmal már történt foglalás.") {
+        }).then(function (data) {
+          if (
+            data.data ===
+            "A foglalás már létezik erre a telefonszámra és időpontra."
+          ) {
             $("#reservationModalLabel").text("Error");
-            $(".modal-body").text(response.data);
+            $(".modal-body").text(data.data);
           } else {
             $("#reservationModalLabel").text("Reservation Confirmation");
             $(".modal-body").text("Köszönjük a foglalást!");
@@ -217,3 +221,5 @@
       },
     ]);
 })(window, angular);
+
+// F.A.Q
