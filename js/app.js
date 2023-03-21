ddd3c8f2ec2b587151cff6a9fdf21de3351659bd
Adam <racsko.adamakos-2018@keri.mako.hu>
2023-03-21 13:53:08 +0100

    asd

--- js/app.js
+++ js/app.js
@@ -68,6 +68,7 @@
         // On before transaction
         let isFirstRun = true;
         $transitions.onBefore({}, function (transition) {
+          window.scrollTo(0,0);
           return $timeout(function () {
             if (isFirstRun) {
               isFirstRun = false;
@@ -77,6 +78,7 @@
             return true;
           }).catch((e) => console.log(e));
         });
+
 
         // Set global variables
         $rootScope.state = { id: null, prev: null };
