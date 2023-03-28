a676dfb2db058c935019c9f7d02ff024753f6ca3
Konyhasneni22 <balla.istvan-2018@keri.mako.hu>
2023-03-28 11:52:41 +0200

    email

--- php/send_email.php
+++ php/send_email.php
@@ -7,31 +7,29 @@ use PHPMailer\PHPMailer\SMTP;
 require '../components/PHPMailer/src/PHPMailer.php';
 require '../components/PHPMailer/src/Exception.php';
 require '../components/PHPMailer/src/SMTP.php';
-function send_email($to, $subject, $message, $e) {
+
+function send_email($to, $subject, $message) {
     $mail = new PHPMailer(true);
-    $mail->SMTPDebug = SMTP::DEBUG_SERVER;
 
     try {
         $mail->isSMTP();
-        $mail->Host = 'smtp.freemail.hu';
+        $mail->Host = 'smtp.freemail.hu'; // vagy 'mail.gmail.hu'
         $mail->SMTPAuth = true;
-        $mail->Username = 'Marianna.katalin@freemail.hu';
-        $mail->Password = 'Asderfucker2@';
+        $mail->Username = 'oinosetterem2023@freemail.hu';
+        $mail->Password = 'Futy132aSfhWeGG';
         $mail->SMTPSecure = 'STARTTLS';
         $mail->Port = 587;
-    
-        $mail->setFrom('Marianna.katalin@freemail.hu');
+
+        $mail->setFrom('oinosetterem2023@freemail.hu');
         $mail->addAddress($to);
         $mail->isHTML(true);
-    
+
         $mail->Subject = $subject;
         $mail->Body    = $message;
-    
+
         $mail->send();
         return true;
     } catch (Exception $e) {
-        $error_message = 'Email could not be sent. Error message: ' . $mail->ErrorInfo;
-        error_log($error_message);
         return false;
     }
 }
@@ -41,28 +39,10 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $subject = 'Sikeres rendelés';
     $message = 'Köszönjük a rendelését! A rendelés részletei...';
 
-    $mail = new PHPMailer(true);
-
-    try {
-        $mail->isSMTP();
-        $mail->Host = 'smtp.freemail.hu'; // vagy 'mail.gmail.hu'
-        $mail->SMTPAuth = true;
-        $mail->Username = 'Marianna.katalin@freemail.hu';
-        $mail->Password = 'Asderfucker2@';
-        $mail->SMTPSecure = 'STARTTLS';
-        $mail->Port = 587;
-
-        $mail->setFrom('pisti1231212@gmail.com');
-        $mail->addAddress($to);
-        $mail->isHTML(true);
-
-        $mail->Subject = $subject;
-        $mail->Body    = $message;
-
-        $mail->send();
+    if (send_email($to, $subject, $message)) {
         echo 'Email sent successfully';
-    } catch (Exception $e) {
-        echo 'Email could not be sent. Error message: ', $mail->ErrorInfo;
+    } else {
+        echo 'Email could not be sent';
     }
 }
-
+?>
